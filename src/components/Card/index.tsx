41ccc3a355c2a7ea10e57de0f81461b8880fbabe
4lessandrodev <alessandroadm@live.com>
2024-04-11 18:07:51 -0300

    feat: added event sample to test

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -2,7 +2,9 @@ import { useEffect, useState } from 'react';
 import { UID } from 'rich-domain';
 import CreateUser from '../../domain/factory';
 import User from '../../domain/user.aggregate';
-import '../../styles/Card.module.css';
+import style from '../../styles/Card.module.css';
+import { Context } from 'rich-domain';
+const context = Context.events();
 
 interface Props {
 	id?: UID;
@@ -15,12 +17,18 @@ export default function Card({ id, name }: Props) {
 
 	useEffect(() => {
 		setUser(CreateUser({ name, id }));
-	}, [id, name])
+	}, [id, name]);
+
+	const handler = () => {
+		const occurredAt = new Date().toISOString();
+		context.dispatchEvent('SIGNUP', user?.toObject() ?? null, occurredAt);
+	}
 
 	return (
-		<div className="Card">
-			<div className="Header"><p>Id: {user?.id.value()}</p></div>
-			<div className="Body"><p>Name: {user?.get('name').get('value')}</p></div>
+		<div className={style.Card} onClick={() => handler()}>
+			<div className={style.Header}><p>Id: {user?.id.value()}</p></div>
+			<div className={style.Body}><p>Name: {user?.get('name').get('value')}</p></div>
+			<p className={style.Label}>Click to dispatch Event</p>
 		</div>
 	)
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,30 @@
 import type { NextPage } from 'next'
 import Card from '../components/Card'
 import styles from '../styles/Home.module.css'
+import { useEffect, useState } from 'react'
+import { Context } from 'rich-domain';
+const context = Context.events();
 
 const Home: NextPage = () => {
+
+  const [events, setEvents] = useState<any[]>([]);
+
+  useEffect(() => {
+    context.subscribe('SIGNUP', (e) => {
+      setEvents((old) => [...old, e.detail]);
+    });
+  }, []);
 
   return (
     <div className={styles.container}>
       <Card name="Jane Doe" />
+      <div>
+        <ul>
+          {events.map((evt: any, i) => (
+            <li key={i}>{`Event: ${evt?.[1]}`}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   )
 }
